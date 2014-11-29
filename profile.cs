3593c985690aa9b5f3a712c0365af1c60f2b96dd
tuanguyen94 <tuanguyen94@gmail.com>
2014-11-29 11:33:33 +0700

    Project 1 - changeIP - C#
    Project 1 - Nguyễn Mạnh Tuấn - Phần mèm thay đổi IP, Subnet, gateway,
    DNS, Hostname - Đại học bách khoa Hà Nội

---
+++ profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_final
+{
+    class profile
+    {
+        public string Address { get; set; }
+        public string IP { get; set; }
+        public string Subnet { get; set; }
+        public string Gateway { get; set; }
+        public string DNS { get; set; }
+        public string Hostname { get; set; }
+    }
+}
